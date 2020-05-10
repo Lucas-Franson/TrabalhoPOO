e386a3e7cafbc254824e8ceeb08bcac20edc35a4
GuilhermeEV <guilhermeev3@gmail.com>
2020-05-10 14:39:09 -0300

    <Arquivos atualizados>

---
+++ src/Exercicios/conta.java
@@ -0,0 +1,45 @@
+package Exercicios;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 00098519
+ */
+public class conta {
+   public String NomeTitular;
+   public int Numero;
+   public String Agencia;
+   public double Saldo;
+   public data DataAbertura;
+   
+   public double Saca(double Valor){
+       return Saldo - Valor;
+   }
+   public double Deposita(double Valor){
+       this.Saldo = this.Saldo + Valor;
+       return this.Saldo;
+   }
+   public double CalculaRendimento(){
+       return Saldo + (Saldo * 0.01);
+   }
+   
+   public String recuperaDadosParaImpressao(){
+       String dados = "Titular: " + this.NomeTitular;
+       dados += "\nNúmero: " + this.Numero;
+       dados += "\nAgência: " + this.Agencia;
+       dados += "\nSaldo: " + this.Saldo;
+       //dados += "\nDia: " + this.DataAbertura.dia;
+       //dados += "\nMês: " + this.DataAbertura.mes;
+       //dados += "\nAno: " + this.DataAbertura.ano;
+       dados += "\nData de abertura: " + this.DataAbertura.formatada();
+       //Quando é executado sem valores o dia, mes e ano, é atribuído zero.
+       dados += "\nCálculo do Rendimento Mensal: " + this.CalculaRendimento();
+       return dados;
+   }
+}
+ 
---
+++ src/Exercicios/data.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicios;
+
+/**
+ *
+ * @author 00098519
+ */
+public class data {
+    int dia;
+    int mes;
+    int ano;
+    
+    public String formatada(){
+        return dia + "/" + mes +"/" + ano;
+    }
+}
